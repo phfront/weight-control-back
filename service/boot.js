70bfd6bd8889da160588d8f0b00cbcd8f027557e
Pedro Henrique Silva Rosa <pedro.rosa@bcocorporate.ad>
2020-10-22 11:54:50 -0300

    heroku

--- service/boot.js
+++ service/boot.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const jwt = require('_helpers/jwt');
 const errorHandler = require('_helpers/error-handler');
 
@@ -10,24 +9,22 @@ module.exports = (err) => {
         return console.log(err);
     }
 
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(bodyParser.json());
     app.use(cors());
 
     // use JWT auth to secure the api
     app.use(jwt());
 
     // api routes
-    app.use('/user', require('../user/user.controller'));
-    app.use('/deck', require('../deck/deck.controller'));
-    app.use('/mycards', require('../mycards/mycards.controller'));
+    app.use('/api/user', require('../user/user.controller'));
+    app.use('/api/deck', require('../deck/deck.controller'));
+    app.use('/api/mycards', require('../mycards/mycards.controller'));
 
     // global error handler
     app.use(errorHandler);
 
     // start server
-    const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
-    const server = app.listen(port, function () {
+    const port = process.env.PORT || 4000;
+    app.listen(port, function () {
         console.log('Server listening on port ' + port);
     });
 
